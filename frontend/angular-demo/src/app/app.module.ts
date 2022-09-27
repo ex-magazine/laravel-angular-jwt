15649ab1232928dc964c62a64270b7524be01c0e
Tommy <thuy.tranthanh.bd@gmail.com>
2022-09-27 12:23:14 +0700

    update

--- frontend/angular-demo/src/app/app.module.ts
+++ frontend/angular-demo/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
