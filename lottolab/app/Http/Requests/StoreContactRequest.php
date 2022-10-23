57390d86ca54734d771a11caf7ace435bc5c6138
Tommy <thuy.tranthanh.bd@gmail.com>
2022-10-23 15:23:07 +0700

    update

---
+++ lottolab/app/Http/Requests/StoreContactRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function messages()
+    {
+        return ['id.required' => 'is required'];
+    }
+
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'id' => 'Contact id',
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            // logic
+        });
+    }
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'slug' => Str::slug($this->slug),
+        ]);
+    }
+}
